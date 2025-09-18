ad012937edfad5557bc02fd1f550f683af39d4da
juniafersa-blip <juniafer.sa@gmail.com>
2025-09-18 17:06:28 +0000

    Criei o desafio Super Trunfo Nível Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,16 +7,22 @@ char codigo1[4] = "B01";
 char nomecidade1[20] = "Salvador";
 int populacao1 = 2418005;
 float areakm21 = 693.8;
-float pibembilhoes1 = 62.95;
+float pibembilhoes1 = 62954487000.00;
 int pontosturisticos1 = 10;
+float densidadepopulacional1 = (float)populacao1 / areakm21;
+float pibpercapita1 = (float)pibembilhoes1 / populacao1;
+
 
 char estado2[10] = "A";
 char codigo2[4] = "A01";
 char nomecidade2[20] = "Manaus";
 int populacao2 = 2279686;
 float areakm22 = 11401.092;
-float pibembilhoes2 =  103.2;
+float pibembilhoes2 =  103281436000.00;
 int pontosturisticos2 = 7;
+float densidadepopulacional2 = (float)populacao2 / areakm22;
+float pibpercapita2 = (float)pibembilhoes2 / populacao2;
+
 
 printf("Carta 1\n");
 printf("Digite o estado da carta 1 ( A - H): \n");
@@ -33,6 +39,8 @@ printf("Digite o PIB em bilhões da carta 1: \n");
 scanf("%f", &pibembilhoes1);
 printf("Digite o número de principais pontos turísticos da carta 1: \n");
 scanf("%d", &pontosturisticos1);
+printf("A densidade populacional de Salvador é: %.2f hab/km2\n", densidadepopulacional2);
+printf("O PIB per capita de Salvador é: %.2f reais\n", pibpercapita1);
 
 printf("Carta 2\n");
 printf("Digite o estado da carta 2 (A - H)");
@@ -49,6 +57,8 @@ printf("Digite o PIB em bilhões da carta 2: \n");
 scanf("%f", &pibembilhoes2);
 printf("Digite o número de principais pontos turísticos da carta 2: \n");
 scanf("%d", &pontosturisticos2);
+printf(" A densidade populacional de Manaus é: %.2f hab/km2\n", densidadepopulacional1);
+printf("O PIB per capita de Manaus é: %.2f reais\n ", pibpercapita1);
 
 
 return 0;
